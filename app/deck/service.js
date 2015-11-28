f8645802dc34acc4240c0376ce7407904e837085
hexmind <hexmind@hexmind.pl>
2015-11-28 17:00:13 +0100

    md icons

--- app/deck/service.js
+++ app/deck/service.js
@@ -1,25 +1,39 @@
 'use strict';
 angular
   .module('MyApp')
-  .service("deckService", function(deckConstants){
+  .service("deckService", function(deckConstants) {
 
-  function getAllCards(settings){
-    var lang = settings.language;
-    return deckConstants[lang].basic;
-  }
+    function getAllCards(settings) {
+      var lang = settings.language;
+      return deckConstants[lang].basic;
+    }
 
-  function isEnding(e){
-    return e.group === 'ending';
-  }
+    function isEnding(e) {
+      return e.group === 'ending';
+    }
 
-  function createCard(data){
-    return _.assign(data, {
-      visible: true,
-      icon: getIcon(data.group)
-    });
-  }
+    function createCard(data) {
+      return _.assign(data, {
+        visible: true,
+        icon: getMdiIcon(data.group)
+      });
+    }
 
-  function getIcon(tag){
+    function getMdiIcon(tag) {
+      var mapping = {
+        item: 'book',
+        character: 'account',
+        event: 'calendar',
+        places: 'map-marker',
+        aspect: 'star',
+        ending: 'check'
+      }
+      var value = mapping[tag] || 'help';
+      return "mdi mdi-" + value;
+    }
+
+    // deprecated
+    function getIcon(tag) {
       var mapping = {
         item: 'book',
         character: 'face',
@@ -31,12 +45,12 @@ angular
       return mapping[tag] || 'help';
     }
 
-  this.drawCards = function(settings){
-    var deck = getAllCards(settings);
-    var cards = settings.cards;
-    var ending = _.chain(deck).shuffle().find(isEnding).value();
-    var hand = _.chain(deck).shuffle().reject(isEnding).take(cards).sortByAll(['interrupt', 'group']).value();
-    hand.push(ending);
-    return _.map(hand, createCard);
-  }
-})
+    this.drawCards = function(settings) {
+      var deck = getAllCards(settings);
+      var cards = settings.cards;
+      var ending = _.chain(deck).shuffle().find(isEnding).value();
+      var hand = _.chain(deck).shuffle().reject(isEnding).take(cards).sortByAll(['interrupt', 'group']).value();
+      hand.push(ending);
+      return _.map(hand, createCard);
+    }
+  })
